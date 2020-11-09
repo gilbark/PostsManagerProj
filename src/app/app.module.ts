7cfdd3984d7e37b7a92b694e3bc530cb82955223
GILB <barkan.gil@gmail.com>
2020-11-09 18:03:42 +0200

    Front end module encapsulation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { PostsModule } from './posts/posts.module';
+import { AngularMaterialModule } from "./angular-material.module";
 import { ErrorComponent } from "./error.component";
 import { ErrorInterceptor } from "./error-interceptor";
 import { AuthInterceptor } from "./auth/auth-interceptor";
@@ -6,31 +8,16 @@ import { LoginComponent } from "./auth/login/login.component";
 import { HeaderComponent } from "./header/header.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatInputModule } from "@angular/material/input";
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatExpansionModule,
-  MatPaginatorModule,
-  MatProgressSpinnerModule,
-  MatToolbarModule,
-  MatDialogModule,
-} from "@angular/material";
-import { PostListComponent } from "./posts/post-list/post-list.component";
-import { PostsService } from "./services/posts.service";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent,
     HeaderComponent,
-    PostListComponent,
     LoginComponent,
     SignupComponent,
     ErrorComponent,
@@ -39,17 +26,10 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatDialogModule,
     HttpClientModule,
+    AngularMaterialModule,
+    PostsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
